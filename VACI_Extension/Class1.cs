ca5613fdd9fabbff7b48b19c4bae079b82ec1782
N. van Oosten <nvanoosten.1992@gmail.com>
2017-12-10 15:53:17 +0100

    Add project files.

---
+++ VACI_Extension/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VACI_Extension
+{
+    public class Class1
+    {
+    }
+}
